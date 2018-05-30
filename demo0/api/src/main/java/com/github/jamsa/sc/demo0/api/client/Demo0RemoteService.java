50828749e68fc2fb49afcd30c64acaca7c148c4e
zhujie <jiemao@gmail.com>
2018-05-30 19:32:26 +0800

    以demo0作为服务提供方，提供hello服务 调整api模块的依赖配置，它只使用spring-web库中的注解。

--- demo0/api/src/main/java/com/github/jamsa/sc/demo0/api/client/Demo0RemoteService.java
+++ demo0/api/src/main/java/com/github/jamsa/sc/demo0/api/client/Demo0RemoteService.java
@@ -1,8 +1,13 @@
 package com.github.jamsa.sc.demo0.api.client;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  * Created by zhujie on 2018/5/29.
  */
 public interface Demo0RemoteService {
-    String hello(String name);
+    @RequestMapping(value="/hello",method= RequestMethod.GET)
+    String hello(@RequestParam String name);
 }
--- demo0/service/src/main/java/com/github/jamsa/demo0/controller/Demo0Controller.java
+++ demo0/service/src/main/java/com/github/jamsa/demo0/controller/Demo0Controller.java
@@ -4,6 +4,8 @@ import com.github.jamsa.sc.demo0.api.client.Demo0RemoteService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by zhujie on 2018/5/29.
@@ -11,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController("/demo0")
 public class Demo0Controller implements Demo0RemoteService {
 
     @Override
